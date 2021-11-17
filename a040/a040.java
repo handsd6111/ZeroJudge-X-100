60deb23961bc33a6e1c4d3113ed4a22591367560
handsd6111 <handsd6111@gmail.com>
2021-11-17 20:53:07 +0800

    add a040

---
+++ a040/a040.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class a040 {
+    public static void main(String[] argv) {
+        Scanner sc = new Scanner(System.in);
+        int min = sc.nextInt(), max = sc.nextInt();
+        for (int i = min; i < max; i++) {
+            String tmp = String.valueOf(i);
+            int total = 0;
+            for(int j = 0;j < tmp.length(); j++) {
+                total += Math.pow(Integer.parseInt(String.valueOf(tmp.charAt(j))), tmp.length());
+            }
+            if(total == i) {
+                System.out.printf("%d ", i);
+            }
+        }
+    }
+}
